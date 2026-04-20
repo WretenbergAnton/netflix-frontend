501171bb09bcf7749ee3fe6e08ec9771965c0912
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2026-04-20 14:21:04 +0200

    Set up Apollo Client with JWT auth header

---
+++ frontend/src/apollo.js
@@ -0,0 +1,23 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: 'https://netflix-graphql-api-production.up.railway.app/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('jwt')
+  return {
+    headers: {
+      ...headers,
+      ...(token ? { authorization: `Bearer ${token}` } : {}),
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
+export default client
